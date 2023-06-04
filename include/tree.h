1c35661027089e9304730f1ee437c3145ca74f15
NikitaKrasnov12 <116708902+NikitaKrasnov12@users.noreply.github.com>
2023-06-04 22:37:56 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -3,61 +3,59 @@
 #define INCLUDE_TREE_H_
 #include <vector>
 
-class Tree {
- private:
-    struct Node {
-        std::vector<Node*> otherNum;
+struct Node {
         char num;
+        std::vector<Tree*> otherNum;
     };
-    std::vector<std::vector<char>> rearrangements;
-    Node* root;
-    std::vector<char> manyNum;
-    void createRearrangementTree( Node* node1, std::vector<char> other) {
-        if (other.empty()) {
-            return;
-        }
-        int j = 0;
-        for (int i = 0; i < other.size(); i++) {
-            Node* root1 = new Node;
-            root1->num = other[i];
-            node1->otherNum.push_back(root1);
-            std::vector<char> notall;
-            for (char k : other) {
-                if (k == other[i]) {
-                    continue;
-                }
-                notall.push_back(k);
-            }
-            createRearrangementTree(root1, notall);
-        }
+class Tree {
+private:
+    Node* nodeOp;
+    explicit Tree(char numSet) {
+        nodeOp = new Node;
+        nodeOp->num = numSet;
     }
-    void createRearrangements(Node* node2, std::vector<char> nowVal) {
-        if (node2->num) {
-            nowVal.push_back(node2->num);
-        }
-        if (node2->otherNum.empty()) {
-            rearrangements.push_back(nowVal);
-            return;
-        }
-        for (int i = 0; i < node2->otherNum.size(); i++) {
-            createRearrangements(node2->otherNum[i], nowVal);
+    void create(const std::vector<char>& other) {
+        for (int i = 0; i < other.size(); i++) {
+            std::vector<char> other_t = other;
+            nodeOp->otherNum.push_back(new Tree(other_t[i]));
+            other_t.erase(other_t.begin() + i);
+            nodeOp->otherNum[i]->create(other_t);
         }
     }
 
- public:
-  explicit Tree(const std::vector<char> other) {
-    manyNum = other;
-    std::vector<char> goVec;
-    root = new Node;
-    root->num = 0;
-    createRearrangements(root, goVec);
-    createRearrangementTree( root, manyNum);
-  }
-  std::vector<char> getOrdPerm(int n)const {
-        if (n - 1 > rearrangements.size()) {
-            return std::vector<char>();
-        }
-        return rearrangements[n - 1];
-  }
+public:
+    explicit Tree(const std::vector<char>& other) {
+        nodeOp = new Node;
+        nodeOp->num = '*';
+        create(other);
+    }
+    char get_NumSet() const {
+        return nodeOp->num;
+    }
+    Tree& operator[](int n) const {
+        return *nodeOp->otherNum[n];
+    }
+    int get_Size() const {
+        return nodeOp->otherNum.size();
+    }
 };
+
+
+int fact(int n) {
+    if (n > 1)
+        return n * fact(n - 1);
+    return 1;
+}
+std::vector<char> getPerm(const Tree& tree, int n) {
+    if (tree.get_Size() == 0 || n > fact(tree.get_Size()))
+        return {};
+    if (tree.get_NumSet() == '*')
+        n--;
+    std::vector<char> resultat;
+    int other_k = fact(tree.get_Size() - 1);
+    resultat.push_back(tree[n / other_k].get_NumSet());
+    std::vector<char> k = getPerm(tree[n / other_k], n % other_k);
+    resultat.insert(resultat.end(), k.begin(), k.end());
+    return resultat;
+}
 #endif  // INCLUDE_TREE_H_
