64d2e5368e47f0008549a2601e432370f23800d5
NikitaKrasnov12 <116708902+NikitaKrasnov12@users.noreply.github.com>
2023-06-04 22:45:28 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -40,22 +40,4 @@ class Tree {
     }
 };
 
-
-int fact(int n) {
-    if (n > 1)
-        return n * fact(n - 1);
-    return 1;
-}
-std::vector<char> getPerm(const Tree& tree, int n) {
-    if (tree.get_Size() == 0 || n > fact(tree.get_Size()))
-        return {};
-    if (tree.get_NumSet() == '*')
-        n--;
-    std::vector<char> resultat;
-    int other_k = fact(tree.get_Size() - 1);
-    resultat.push_back(tree[n / other_k].get_NumSet());
-    std::vector<char> k = getPerm(tree[n / other_k], n % other_k);
-    resultat.insert(resultat.end(), k.begin(), k.end());
-    return resultat;
-}
 #endif  // INCLUDE_TREE_H_
