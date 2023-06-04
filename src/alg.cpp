90fa150ebda90d55f99a68c89e504cf8ce62eec4
NikitaKrasnov12 <116708902+NikitaKrasnov12@users.noreply.github.com>
2023-06-04 22:37:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,6 +6,20 @@
 #include <vector>
 #include  "tree.h"
 
+int fact(int n) {
+    if (n > 1)
+        return n * fact(n - 1);
+    return 1;
+}
 std::vector<char> getPerm(const Tree& tree, int n) {
-  return tree.getOrdPerm(n);
+    if (tree.get_Size() == 0 || n > fact(tree.get_Size()))
+        return {};
+    if (tree.get_NumSet() == '*')
+        n--;
+    std::vector<char> resultat;
+    int other_k = fact(tree.get_Size() - 1);
+    resultat.push_back(tree[n / other_k].get_NumSet());
+    std::vector<char> k = getPerm(tree[n / other_k], n % other_k);
+    resultat.insert(resultat.end(), k.begin(), k.end());
+    return resultat;
 }
